830a48dce46f617ff219ac43997a09b470a39c51
wpellas <william.pellas@hotmail.com>
2023-02-04 08:21:09 +0100

    1.1.0

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,48 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searchfield: '',
-        }
-    }
-
-    componentDidMount() {
+function App() {
+    const [robots, setRobots] = useState([]);
+    const [searchfield, setSearchfield] = useState('');
+    
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => this.setState({ robots: users }));
-    }
+            .then(users => {setRobots(users)});
+    },[])
 
-    onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value })
-        }
-    render() {
-        const { robots, searchfield } = this.state;
-        const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
-        })
-        return !robots.length ?
-            <h1>Loading</h1> : 
-            (
-                <div  className='tc'>
-                    <h1 className='f1'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
-                    <Scroll>
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value)
+    }
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase())
+    })
+    return !robots.length ?
+        <h1>Loading</h1> : 
+        (
+            <div>
+                <div  className='tc container center'>
+                    <h1 className='f1'>{'ROBOFRIENDS'}</h1>
+                    <SearchBox searchChange={onSearchChange} />
                         <ErrorBoundry>
                             <CardList robots={filteredRobots} />
                         </ErrorBoundry>
-                    </Scroll>
-                </div>   
-            );
-        } 
-    }
+
+                </div>
+                <div className="footer shadow-5">
+                        <p className="footerText">This project was made by <a href="https://williampellas.com/">William Pellas</a> as a course project for Zero to Mastery 'The Complete Web Developer in 2023: Zero to Mastery'. Check out the <a href="https://github.com/wpellas/robofriends">GitHub repo</a>.</p>
+                </div>
+            </div>   
+        );
+    } 
 
 export default App;
